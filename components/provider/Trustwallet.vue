19d622461e25b6e1b41dfd2d241ac8641ebd124c
BossSuricate <suricategamingytb@gmail.com>
2021-07-31 16:01:12 +0200

    J'avais oublier

--- components/provider/Trustwallet.vue
+++ components/provider/Trustwallet.vue
@@ -19,12 +19,46 @@
 <script lang="ts">
 import Vue from 'vue'
 import { mapActions } from 'vuex'
+import { ethers } from 'ethers'
 
-export default Vue.extend({
+export default Vue.extend<any, any, any, any>({
   methods: {
     async handleClick() {
-      // @TODO Rajouter un syteme qui affiche correctement les erreurs interne
-      await this.useWallet('metamask')
+      const result: {
+        ok: boolean
+        provider?: ethers.providers.Web3Provider | undefined
+        error?: { code: number; message: string } | undefined
+      } = await this.useWallet('metamask')
+      if (!result.ok) {
+        const title = 'Erreur Connexion !'
+        let text = "Nous n'avons pas reussi a vous connecter !"
+
+        switch (result.error?.code) {
+          case 4001:
+            text = 'Vous avez refuser de vous connecter avec metamask !'
+            break
+          case 1:
+            text =
+              'Nous avons trouver aucun provider (window.ethereum) veuiller verifier que vous avez bien MetaMask installer !'
+            break
+          case 2:
+            text =
+              'Desoler la blockchain est incorrecte. Veuiller selectionner Polygon !'
+            break
+
+          default:
+            break
+        }
+
+        this.$vs.notification({
+          position: 'top-right',
+          color: 'danger',
+          icon: `<i class='bx bxs-error-circle'></i>`,
+          duration: 6000,
+          title,
+          text,
+        })
+      }
     },
     ...mapActions('wallet', ['useWallet']),
   },
