da3b29d4a4b6940b62b78e8694af9508cc23ff78
BossSuricate <suricategamingytb@gmail.com>
2021-08-19 19:31:24 +0200

    tg le bruh

--- pages/devs.vue
+++ pages/devs.vue
@@ -44,6 +44,68 @@
           Le meilleur devellopeur du monde !
         </p>
       </div>
+      <div
+        class="
+          flex flex-col
+          items-center
+          p-4
+          dark:bg-dark-1
+          bg-white
+          rounded-lg
+        "
+      >
+        <img src="/devs/stebz.jpeg" class="rounded-full h-36 w-36" alt="" />
+        <h2 class="flex items-center mt-4 flex-bold text-lg dark:text-white">
+          Stebz
+        </h2>
+        <div
+          class="flex mt-2 text-gray-400 dark:text-white justify-center w-full"
+        >
+          <vs-tooltip>
+            <div class="mx-2">💻</div>
+            <template #tooltip>Future Devellopeur </template>
+          </vs-tooltip>
+          <div>/</div>
+          <vs-tooltip>
+            <div class="mx-2">🎤</div>
+            <template #tooltip> Community Manager </template>
+          </vs-tooltip>
+          <div>/</div>
+          <vs-tooltip>
+            <div class="mx-2">🔑</div>
+            <template #tooltip> Administrateur </template>
+          </vs-tooltip>
+        </div>
+        <p class="text-gray-400 text-xs mt-2">
+          Il est la pour la mixiter de la communauter.
+        </p>
+      </div>
+      <div
+        class="
+          flex flex-col
+          items-center
+          p-4
+          dark:bg-dark-1
+          bg-white
+          rounded-lg
+        "
+      >
+        <img src="/devs/wayager.jpg" class="rounded-full h-36 w-36" alt="" />
+        <h2 class="flex items-center mt-4 flex-bold text-lg dark:text-white">
+          Wayager⁷
+        </h2>
+        <div
+          class="flex mt-2 text-gray-400 dark:text-white justify-center w-full"
+        >
+          <vs-tooltip>
+            <div class="mx-2">🎨</div>
+            <template #tooltip>Designer </template>
+          </vs-tooltip>
+        </div>
+        <p class="text-gray-400 text-xs mt-2">
+          Le createur du design du token STI.
+        </p>
+      </div>
     </div>
   </div>
 </template>
