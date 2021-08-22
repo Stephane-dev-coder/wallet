60a5c632860c99ac01d796a3edbd1262c69c98fc
BossSuricate <suricategamingytb@gmail.com>
2021-08-22 21:44:21 +0200

    Let's go

--- pages/faq.vue
+++ pages/faq.vue
@@ -27,12 +27,32 @@ export default Vue.extend({
     return {
       questions: [
         {
-          q: 'Est ce que STI est une vrai monnaie ?',
-          a: "Oui. Le STI tout comme le Bitcoin ou l'ethereum est une vraie monnaie",
+          q: "Ou est ce que je pourrais avoir plus d'information sur le token STI ?",
+          a: 'Vous trouverez toutes les informations que vous rechercher dans le token metrics trouvable dans le bas de page',
         },
         {
-          q: 'Est ce que STI est une vrai monnaie ?',
-          a: "Oui. Le STI tout comme le Bitcoin ou l'ethereum est une vraie monnaie",
+          q: 'A quoi sert le STI ?',
+          a: "Le STI est un token communautaire. Il reunnie toutes les personnes qui veulent cree des petit projet et se marrer. Un seul mot d'ordre cree et partager !",
+        },
+        {
+          q: 'Est ce que je peux devenir riche grace au STI ?',
+          a: "Oui... Mais cela ne dependra uniquement de l'investissement de la communauter au projet",
+        },
+        {
+          q: 'Comment marche le wallet ?',
+          a: 'Vous avez a votre disposition une suite de video tutoriel qui devrais vous aidez a performer les actions les plus courantes autour du STI',
+        },
+        {
+          q: 'Comment puis-je contribuer ?',
+          a: "Vous pouvez contribuer de 1000 facons differentes, en parler, cree des memes, aider au developemment, proposer des idees, etc. Soyez sur d'une chose cette communauter ne connait pas de limite alors si vous avez du temps CONTRIBUER !",
+        },
+        {
+          q: 'Comment je peux acheter du STI ?',
+          a: 'Vous pouvez acheter des STI sur SushiSwap. Si vous ne connaisser pas bien cette plateforme laisser nous vous guider grace a nos tutoriels que vous trouverez en bas de la page !',
+        },
+        {
+          q: "J'ai une autre question ou est ce que je peux la poser ?",
+          a: 'Discord est votre meilleur amis pour poser toute question sur le projet !',
         },
       ],
     }
