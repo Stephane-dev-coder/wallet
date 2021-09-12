5a6159e4d44281e43166d024c41452b4dad1eaa1
stebz <stephane.zayat@limayrac.fr>
2021-09-12 12:02:34 +0200

    f11

--- pages/stats.vue
+++ pages/stats.vue
@@ -3,7 +3,7 @@
     <div class="mt-10">
       <h2 class="text-3xl dark:text-gray-200">Statistiques</h2>
       <h2 class="text-xs mt-1 text-gray-600 dark:text-gray-400">
-        Toutes les statistiques pas (encore) en temps reel
+        Toutes les statistiques pas (encore) en temps réel.
       </h2>
     </div>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
